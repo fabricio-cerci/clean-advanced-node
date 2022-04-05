892c7d1e5ccf81942afbead9a8020bae6e3bddbd
Fabrício Cerci <fabriciocerci@gmail.com>
2022-04-04 22:57:38 -0400

    ensure FacebooApi returns undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '1574663639575436',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? 'a01598a10ee17777b6d467a20a453c4e'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAAWYJfLgq4wBACxYObujahU5w8CCcNbzMbvOIWlIxJhhCfp7RsS3XRwxQGq8tZAxl6TAvgrxqG5a6upxldmTQZAobW6HRXsSUZAI4qP4Ua53Ewmd85OEgQXCyTX9kBrKc0hZCQ6D81WSdyHFSo3QKx9OTXqWldWh8uOZB0UZA0YzS4xqL6hQCboum3Ov5niYqvH0JIQJ57fAZDZD' })
+
+    expect(fbUser).toEqual({
+      facebookId: '104913568855260',
+      email: 'fabricio_wxvhugi_teste@tfbnw.net',
+      name: 'Fabricio Teste'
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid_token' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
